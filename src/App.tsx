9988f8d611901baecef507da5b7537675909e2f9
muratavci05 <js.muratavci@gmail.com>
2023-01-10 16:12:49 +0300

    app.tsx modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "./store";
 import { add, remove, toggleCompleted } from "./features/TodoSlice";
+import Marquee from "react-fast-marquee";
+
 
 function App() {
   const todos = useAppSelector((state) => state.todos);
@@ -21,21 +23,55 @@ function App() {
     dispatch(toggleCompleted(id));
   };
   return (
-    <div className="container my-5 col-sm-5">
+    <div className="container my-5 col-sm-5 p-5">
+      
+      <Marquee>
+      <h1 className="titleTodos p-4"><span style={{color:"brown"}}>TypeScript with </span>TODO'S</h1>
+      </Marquee>
+      
       <div className="input-group mb-3">
         <input
-          type="text"
+          name="title"
+          value={title}
           className="form-control"
-          placeholder="Recipient's username"
-          aria-label="Recipient's username"
-          aria-describedby="basic-addon2"
+          placeholder="entry..."
+          
+
+          //currentTarget = dinleyisinin eklendiği öge olması nedeni ile burada kullanılmıştır.
+          onChange={(e) => setTitle(e.currentTarget.value)}
         />
         <div className="input-group-append">
-          <button className="btn btn-outline-primary" type="button">
-            Button
+          <button 
+          className="btn btn-outline-primary" 
+          type="button"
+          onClick={onSave}
+          >
+            Save
           </button>
+         
         </div>
+
+       
       </div>
+      <div>
+      <ul className="" >
+            {todos.map((todo)=>(
+              <li key={todo.id} style={{display:"flex", marginTop:"3px"}}>
+                <span className="form-control "
+                
+                >
+                  {todo.title}
+                </span>
+                <button className="btn btn-outline-primary "
+                onClick={()=>toggle(todo.id)}
+                >
+                  {todo.completed ? "uncompleted" : "Done"}
+                </button>
+                <button className="btn btn-outline-danger" onClick={()=>onDelete(todo.id)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+          </div>
     </div>
   );
 }
