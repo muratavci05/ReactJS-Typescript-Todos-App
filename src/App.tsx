0553e6b20d98f81a62c15b17be288dd629fdbef6
muratavci05 <js.muratavci@gmail.com>
2023-01-10 13:28:56 +0300

    all modified

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import Todo from "./components/Todo";
+import { useState } from "react";
+import { useAppDispatch, useAppSelector } from "./store";
+import  {add, remove, toggleCompleted}  from "./features/TodoSlice";
 
 function App() {
+  const todos = useAppSelector((state) => state.todos);
+  const [title, setTitle] = useState("");
+
+  const dispatch = useAppDispatch();
+
+  const onSave = () => {
+    dispatch(add(title));
+    setTitle("");
+  };
+
+  const  onDelete = (id:string)=> {
+    dispatch(remove(id))
+  }
+
+  const toggle= (id: string) => {
+      dispatch(toggleCompleted(id))
+  }
   return (
-    <div className="App">
-      <Todo/>
+    <div className="App ">
+      <div className="container p-2">
+        <div className="col-md-4 mx-auto">
+
+        <h1 className="todo-s">TODO'S</h1>
+      <input
+      
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.currentTarget.value)}
+      />
+
+      <button className="btn btn-success " onClick={onSave}>Save</button>
+      <ul className="list-unstyled">
+        {todos.map((todo) => (
+          <li  key={todo.id}>
+             <span className="form-control  gy-2">{todo.title}</span> 
+            <button className="btn btn-primary me-2 my-2 " onClick={()=>toggle(todo.id)}>{todo.completed ? "Uncompleted" : "Mark Completed"}</button>
+            <button className="btn btn-danger" onClick={() => onDelete(todo.id)}>Delete</button>
+          
+            </li>
+        ))}
+      </ul>
+      </div>
+      </div>
     </div>
   );
 }
--- src/features/TodoSlice.ts
+++ src/features/TodoSlice.ts
@@ -1,12 +1,12 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import {v4} from "uuid";
-
+import { v4 } from "uuid";
 
 export interface Todo {
   id: string;
   title: string;
   completed: boolean;
 }
+//burada initialState todolardan oluşan bir dizi 
 
 const initialState: Todo[] = [];
 
@@ -14,17 +14,26 @@ const todoSlice = createSlice({
   name: "todos",
   initialState,
   reducers: {
-    add: (state, action: PayloadAction<string>) => {
-      const newTodo = { 
+    //action normalde any gözüküyor "payloadaction type" ile ne tarz bir payload göndereceğimizi ayarladım
+    // bu örneğimizde title  string olduğu için   <string> olarak belirtildi.
+      add: (state, action: PayloadAction<string>) => {
+      const newTodo = {
         id: v4(),
-        title: action.payload, 
+        title: action.payload,
         completed: false,
-    };
+      };
       state.push(newTodo);
     },
-    remove: () => {},
+    remove: (state, action: PayloadAction<string>) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+    toggleCompleted: (state, action: PayloadAction<string>) => {
+      return state.map((todo) =>
+        todo.id === action.payload ? { ...todo, completed: !todo.completed } : todo
+      );
+    },
   },
 });
 
 export default todoSlice.reducer;
-export const { add, remove} = todoSlice.actions;
+export const { add, remove, toggleCompleted } = todoSlice.actions;
